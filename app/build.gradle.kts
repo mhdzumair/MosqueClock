1a0e789034e6fcde3c52f4b0f1b8954b1f33046a
mhdzumair <mhdzumair@gmail.com>
2025-10-10 11:15:27 +0530

    chore: bump version to v1.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.mosque.prayerclock"
         minSdk = 21
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.4.1"
+        versionCode = 11
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
